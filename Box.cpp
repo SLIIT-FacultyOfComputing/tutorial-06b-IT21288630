303e0b7e370efbcf4fb632b8936121549834f2b5
IT21288630 <99311971+IT21288630@users.noreply.github.com>
2022-03-16 06:07:18 +0000

    completed

--- Box.cpp
+++ Box.cpp
@@ -1,4 +1,6 @@
 #include "Box.h"
+#include <iostream>
+using namespace std;
 
 // Implement setters and getters
 void setValues(int l, int w, int h)
@@ -8,12 +10,21 @@ void setValues(int l, int w, int h)
   height = h;
 }
 
-void getValues()
+void getHeight()
 {
-  cout << "Length : " << length << endl;
-  cout << "Width : " << width << endl;
   cout << "Height : " << height << endl;
 }
+
+void getLength()
+{
+  cout << "Length : " << length << endl;
+}
+
+void getWidth()
+{
+   cout << "Width : " << width << endl;
+}
+
 // Implemenet the calcVolume() unction
 int Box::calcVolume() {
   return length * width * height;
--- Box.h
+++ Box.h
@@ -7,7 +7,9 @@ class Box {
        // write prototypes of setters for length, width and height
        void setValues(int l, int w, int h);
        // write prototypes of getters for length, width and height
-       void getValues();
- 
+       void getHeight();
+       void getLength();
+       void getWidth();
+
        int calcVolume();
 };
