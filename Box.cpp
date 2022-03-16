b557356e9271f3b52d586d4bacf690acb0f2b35f
IT21288630 <99311971+IT21288630@users.noreply.github.com>
2022-03-16 05:50:10 +0000

    completed

--- Box.cpp
+++ Box.cpp
@@ -1,7 +1,19 @@
 #include "Box.h"
 
 // Implement setters and getters
+void setValues(int l, int w, int h)
+{
+  length = l;
+  width = w;
+  height = h;
+}
 
+void getValues()
+{
+  cout << "Length : " << length << endl;
+  cout << "Width : " << width << endl;
+  cout << "Height : " << height << endl;
+}
 // Implemenet the calcVolume() unction
 int Box::calcVolume() {
 }
