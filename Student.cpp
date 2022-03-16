8c33c67c0c3011140d7a4fefe39f4f3c9fdf7a66
IT21288630 <99311971+IT21288630@users.noreply.github.com>
2022-03-16 05:35:22 +0000

    Completed

--- Student.cpp
+++ Student.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 // Assign studentId and name
-Student::assignDetails(int id, char sName[]) {
+void Student::assignDetails(int id, char sName[]) {
   studentId = id;
   name = sName;
 }
 
 // Display StudentId and Name
-Student::display() {
+void Student::display() {
   cout << "Student Id : " << studentId <<< endl;
   cout << "Name : " << name << endl;
 }
