d6e773a2cc6ea53ea653f64cf4ac700ffc5f8ccf
IT21288630 <99311971+IT21288630@users.noreply.github.com>
2022-03-16 05:45:58 +0000

    completed

--- Box.h
+++ Box.h
@@ -5,7 +5,9 @@ class Box {
        int height;
     public:
        // write prototypes of setters for length, width and height
+       void setValues(int l, int w, int h);
        // write prototypes of getters for length, width and height
+       void getValues();
  
        int calcVolume();
 };
